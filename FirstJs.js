786cf0ba8e7b62dc367b0a4503fe952d84a18c41
razvanmazilu99 <mazilu.razvan.moisil@gmail.com>
2021-07-02 16:50:05 +0300

    Second commit

--- FirstJs.js
+++ FirstJs.js
@@ -4,17 +4,28 @@ var fs = require('fs')
 
 //custom modules
 var ourModule = require('./ourmodule.js')
+var ourModule1 = require('./mathmodule.js')
 
 var server = http.createServer((req, res) => {
-    res.end("Welcome to Atoss World")
+    fs.readFile('./simple.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
 })
 
 server.listen(3000)
 console.log(ourModule.ourFunction())
-fs.readFile('dummy.txt', 'utf-8', (err,data) => {
+
+fs.readFile('dummy.txt', 'utf-8', (err, data) => {
     if(err) {
         console.log("Err while reading the file")
     } else {
         console.log(data)
     }
 })
+
+console.log(ourModule1.add(7, 8))
+console.log(ourModule1.sub(7, 8))
+console.log(ourModule1.mul(7, 8))
+console.log(ourModule1.div(7, 8))
--- express.js
+++ express.js
@@ -1,9 +1,18 @@
 var express = require('express')
 var app = express()
+var ourModule = require('./mathmodule.js')
 
 //default endpoint
 app.get('/', (req, res) => {
     res.send("Welcome to Atoss World")
+})
+
+app.get('/cucu', (req, res) => {
+    res.send("Hello cucu")
+})
+
+app.get('/ex2', (req, res) => {
+    res.send(ourModule.add(7, 8) + " ")
 })
 
 app.listen(3000, () => {
